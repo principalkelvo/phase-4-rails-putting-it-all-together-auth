ce8f72b7e98002cc71bd99e74a2f0907576166f3
principalkelvo <principalkelvo@gmail.com>
2022-10-05 12:03:46 +0300

    sign-up working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,29 @@
 class UsersController < ApplicationController
+    wrap_parameters format: []
+    skip_before_action :authorized, only: :create
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
+    def create
+        user = User.create!(user_params)
+        session[:user_id] = user.id
+        # if user.valid?
+        render json: user, status: :created 
+        # else
+        #     render json: {errors: user.errors.full_messages}, status: :unprocessable_entity
+        #  end
+    end
+
+    def show
+        user = User.find(session[:user_id])
+        render json: user, status: :created
+
+    end
+
+    private
+    def user_params
+      params.permit(:username, :password, :password_confirmation, :image_url, :bio)
+    end
+
+    def render_unprocessable_entity(invalid)
+      render json: {error: invalid.record.errors}, status: :unprocessable_entity
+    end
 end
