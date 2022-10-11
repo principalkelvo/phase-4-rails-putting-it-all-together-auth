13fbae1703118d10676123fd8b05d098fabbb7b8
principalkelvo <principalkelvo@gmail.com>
2022-10-12 00:11:49 +0300

    correction complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,8 @@ class ApplicationController < ActionController::API
   before_action :authorized
 
   def authorized
-    return render json: {error:"Not authorized"}, status: :unauthorized unless session.include? :user_id
+    @current_user = User.find_by(id: session[:user_id]) 
+
+    render json: { errors: ["Not authorized"] }, status: :unauthorized unless @current_user
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,17 +7,17 @@ class SessionsController < ApplicationController
             session[:user_id] = user.id
             render json: user, status: :created
         else
-            render json: {error: "Invalid username or password"}, status: :unauthorized
+            render json: {errors: ["Invalid username or password"]}, status: :unauthorized 
       end
     end
     
       def destroy
-        user = User.find_by(id: session[:user_id])
-        if user
+        # user = User.find_by(id: session[:user_id])
+        # if user
             session.delete :user_id
             head :no_content
-        else
-            render json: {errors: "Not authorized"}, status: :unauthorized
-        end
+        # else
+        #     render json: {errors: "Not authorized"}, status: :unauthorized
+        # end
     end
 end
